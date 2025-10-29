e03f11fcb4d62f8fcdc550656b8bf4a33a5635f8
Antonios Patounis <antonios.patounis@sessionm.com>
2025-10-29 14:30:11 +0100

    client 4

--- main.tf
+++ main.tf
@@ -147,3 +147,36 @@ resource "aws_quicksight_dashboard" "client_3" {
     ]
   }
 }
+
+# 6) third dashboard from the same template
+resource "aws_quicksight_dashboard" "client_4" {
+  aws_account_id      = var.account_id
+  dashboard_id        = "saas-sales-dash-for-client-4"
+  name                = "SaaS Sales Dashboard for Client 4"
+  version_description = "v1 - created from template"
+
+  source_entity {
+    source_template {
+      arn = aws_quicksight_template.from_analysis.arn
+      data_set_references {
+        data_set_placeholder = var.dataset_placeholder
+        data_set_arn         = "arn:aws:quicksight:${var.region}:${var.account_id}:dataset/${var.dataset_id}"
+      }
+    }
+  }
+
+  # keep only your user permission
+  permissions {
+    principal = "arn:aws:quicksight:${var.region}:${var.account_id}:user/default/antonios"
+    actions = [
+      "quicksight:DeleteDashboard",
+      "quicksight:DescribeDashboard",
+      "quicksight:DescribeDashboardPermissions",
+      "quicksight:ListDashboardVersions",
+      "quicksight:QueryDashboard",
+      "quicksight:UpdateDashboard",
+      "quicksight:UpdateDashboardPermissions",
+      "quicksight:UpdateDashboardPublishedVersion",
+    ]
+  }
+}
