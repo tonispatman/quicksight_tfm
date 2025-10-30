53fb8a129a65c0b5bb7cbfe20037457b9f4848c5
Antonios Patounis <antonios.patounis@sessionm.com>
2025-10-30 16:32:52 +0100

    client two

--- main.tf
+++ main.tf
@@ -33,66 +33,36 @@ resource "aws_quicksight_dashboard" "poc_from_imported_template" {
   }
 }
 
-resource "aws_quicksight_dashboard" "poc_from_imported_template_2" {
-  aws_account_id      = var.account_id
-  dashboard_id        = "saas-sales-dash-stg-2"
-  name                = "SaaS Sales Dashboard (STG #2)"
-  version_description = "POC from imported template (second)"
+# resource "aws_quicksight_dashboard" "poc_from_imported_template_2" {
+#   aws_account_id      = var.account_id
+#   dashboard_id        = "saas-sales-dash-stg-2"
+#   name                = "SaaS Sales Dashboard (STG #2)"
+#   version_description = "POC from imported template (second)"
 
-  source_entity {
-    source_template {
-      arn = local.template_arn
+#   source_entity {
+#     source_template {
+#       arn = local.template_arn
 
-      data_set_references {
-        data_set_placeholder = var.dataset_placeholder
-        data_set_arn         = local.dataset_arn
-      }
-    }
-  }
+#       data_set_references {
+#         data_set_placeholder = var.dataset_placeholder
+#         data_set_arn         = local.dataset_arn
+#       }
+#     }
+#   }
 
-  permissions {
-    principal = local.owner_user_arn
-    actions = [
-      "quicksight:DeleteDashboard",
-      "quicksight:DescribeDashboard",
-      "quicksight:DescribeDashboardPermissions",
-      "quicksight:ListDashboardVersions",
-      "quicksight:QueryDashboard",
-      "quicksight:UpdateDashboard",
-      "quicksight:UpdateDashboardPermissions",
-      "quicksight:UpdateDashboardPublishedVersion",
-    ]
-  }
-}
+#   permissions {
+#     principal = local.owner_user_arn
+#     actions = [
+#       "quicksight:DeleteDashboard",
+#       "quicksight:DescribeDashboard",
+#       "quicksight:DescribeDashboardPermissions",
+#       "quicksight:ListDashboardVersions",
+#       "quicksight:QueryDashboard",
+#       "quicksight:UpdateDashboard",
+#       "quicksight:UpdateDashboardPermissions",
+#       "quicksight:UpdateDashboardPublishedVersion",
+#     ]
+#   }
+# }
 
-resource "aws_quicksight_dashboard" "client3" {
-  aws_account_id      = var.account_id
-  dashboard_id        = "client 3"
-  name                = "client 3"
-  version_description = "client 3 dashboard"
 
-  source_entity {
-    source_template {
-      arn = local.template_arn
-
-      data_set_references {
-        data_set_placeholder = var.dataset_placeholder
-        data_set_arn         = local.dataset_arn
-      }
-    }
-  }
-
-  permissions {
-    principal = local.owner_user_arn
-    actions = [
-      "quicksight:DeleteDashboard",
-      "quicksight:DescribeDashboard",
-      "quicksight:DescribeDashboardPermissions",
-      "quicksight:ListDashboardVersions",
-      "quicksight:QueryDashboard",
-      "quicksight:UpdateDashboard",
-      "quicksight:UpdateDashboardPermissions",
-      "quicksight:UpdateDashboardPublishedVersion",
-    ]
-  }
-}
