bd691c3f9fbe6374ca4cd5555d607093ebd7458f
Antonios Patounis <antonios.patounis@sessionm.com>
2025-10-30 12:27:46 +0100

    small change

--- locals.tf
+++ locals.tf
@@ -1,8 +1,6 @@
 locals {
-  template_base_arn = "arn:aws:quicksight:${var.region}:${var.account_id}:template/${var.template_id}"
-  template_arn      = var.template_alias_name != null
-                    ? "${local.template_base_arn}/alias/${var.template_alias_name}"
-                    : local.template_base_arn
+  template_alias_suffix = var.template_alias_name == null ? "" : "/alias/${var.template_alias_name}"
+  template_arn          = "arn:aws:quicksight:${var.region}:${var.account_id}:template/${var.template_id}${local.template_alias_suffix}"
 
   dataset_arn    = "arn:aws:quicksight:${var.region}:${var.account_id}:dataset/${var.dataset_id}"
   owner_user_arn = "arn:aws:quicksight:${var.region}:${var.account_id}:user/default/${var.owner_user_name}"
