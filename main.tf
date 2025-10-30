d694b17e2a95ab0596b540f318296bbce3ed7dab
Antonios Patounis <antonios.patounis@sessionm.com>
2025-10-30 12:40:23 +0100

    second client

--- main.tf
+++ main.tf
@@ -32,3 +32,38 @@ resource "aws_quicksight_dashboard" "poc_from_imported_template" {
     ]
   }
 }
+
+resource "aws_quicksight_dashboard" "second_client" {
+  aws_account_id      = var.account_id
+  dashboard_id        = "second client"
+  name                = "second client"
+  version_description = "second client"
+
+  source_entity {
+    source_template {
+      # If you set var.template_alias_name (e.g., "stg"), local.template_arn resolves to:
+      # arn:aws:quicksight:REGION:ACCOUNT:template/TEMPLATE_ID/alias/stg
+      # Otherwise it’s the base template ARN.
+      arn = local.template_arn
+
+      data_set_references {
+        data_set_placeholder = var.dataset_placeholder
+        data_set_arn         = local.dataset_arn
+      }
+    }
+  }
+
+  permissions {
+    principal = local.owner_user_arn
+    actions = [
+      "quicksight:DeleteDashboard",
+      "quicksight:DescribeDashboard",
+      "quicksight:DescribeDashboardPermissions",
+      "quicksight:ListDashboardVersions",
+      "quicksight:QueryDashboard",
+      "quicksight:UpdateDashboard",
+      "quicksight:UpdateDashboardPermissions",
+      "quicksight:UpdateDashboardPublishedVersion",
+    ]
+  }
+}
