b3bd4b971870ec39f0a21e49a81620bd24eb8e75
Antonios Patounis <antonios.patounis@sessionm.com>
2025-10-31 13:16:34 +0100

    small change

--- main.tf
+++ main.tf
@@ -32,37 +32,3 @@ resource "aws_quicksight_dashboard" "poc_from_imported_template" {
     ]
   }
 }
-
-resource "aws_quicksight_dashboard" "poc_from_imported_template_2" {
-  aws_account_id      = var.account_id
-  dashboard_id        = "saas-sales-dash-stg-2"
-  name                = "SaaS Sales Dashboard (STG #2)"
-  version_description = "POC from imported template (second)"
-
-  source_entity {
-    source_template {
-      arn = local.template_arn
-
-      data_set_references {
-        data_set_placeholder = var.dataset_placeholder
-        data_set_arn         = local.dataset_arn
-      }
-    }
-  }
-
-  permissions {
-    principal = local.owner_user_arn
-    actions = [
-      "quicksight:DeleteDashboard",
-      "quicksight:DescribeDashboard",
-      "quicksight:DescribeDashboardPermissions",
-      "quicksight:ListDashboardVersions",
-      "quicksight:QueryDashboard",
-      "quicksight:UpdateDashboard",
-      "quicksight:UpdateDashboardPermissions",
-      "quicksight:UpdateDashboardPublishedVersion",
-    ]
-  }
-}
-
-
